d4296c99df8f11cd95c6c73bf435ee149dae9aa5
Ayinde Kennedy <35676154+voidl3ss@users.noreply.github.com>
2023-10-16 13:13:50 -0400

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,74 @@
+// mobile search box 
+const searchIcon = document.querySelector('.search-icon');
+const searchBox = document.querySelector('.search-box');
+
+searchIcon.addEventListener('click', () => {
+    console.log('hello world');
+    searchBox.classList.toggle('active')
+})
+
+// header menu 
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+
+menuIcon.addEventListener('click' , () => {
+    if(menuIcon.classList.contains('bx-menu-alt-right')){
+        menuIcon.classList.remove('bx-menu-alt-right');
+        menuIcon.classList.add('bx-chevrons-right');
+        menu.classList.toggle('active');
+    }else{
+        menuIcon.classList.remove('bx-chevrons-right');
+        menuIcon.classList.add('bx-menu-alt-right');
+        menu.classList.toggle('active');
+    }
+})
+
+function navigateTo(url) {
+    window.location.href = url;
+}
+
+// order button
+const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+
+addToCartButtons.forEach(button  => {
+    button.addEventListener('click' , () => {
+        window.location.href = "/2-yourOrder/yourOrder.html";
+    });
+})
+
+// mode change  
+const modeChangeIcon = document.querySelector('.mode-change-icon');
+
+modeChangeIcon.addEventListener('click' , () => {
+    if(modeChangeIcon.classList.contains('bx-moon')){
+        modeChangeIcon.classList.remove('bx-moon');
+        modeChangeIcon.classList.add('bxs-moon');
+        modeChangeIcon.classList.toggle('active');
+        document.body.classList.toggle('dark-theme');
+    }else{
+        modeChangeIcon.classList.remove('bxs-moon');
+        modeChangeIcon.classList.add('bx-moon');
+        modeChangeIcon.classList.toggle('active');
+        document.body.classList.toggle('dark-theme');
+    }
+})
+
+
+//scroll reveal animation 
+// scroller reveal 
+animateElement = (element,origin,delay,distance) => {
+    // Initialize ScrollReveal with options
+    const sr = ScrollReveal({
+        delay: delay,
+        distance: distance,
+        duration: 1000,
+        easing: 'ease',
+        origin: origin
+    });
+  
+    // Reveal the element
+    sr.reveal(element);
+}
+
+animateElement('.hero-content','left','500','500px')
+animateElement('.hero-img','bottom','900','500px')
